2939540c1e4b8c76871d4bcda77fdcdf9e25d4b9
Vinicius Macedo <viniciusmacedo010@outlook.com>
2025-03-26 19:18:49 -0300

    COMMIT

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
         <h1>Bem-vindo a aplicação Laravel Olá Mundo</h1>
         <p>
             Essa aplicação trás de forma breve a estrutura de um projeto Laravel, com rotas, views e controllers.
+            E vai tomando 
         </p>
     </div>
 @endsection
